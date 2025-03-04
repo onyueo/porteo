fe39bda6bb87848437761687af356ffb5168cb42
On Yu <gkc04075@gmail.com>
2025-03-04 22:17:25 +0900

    feat: analytics 연결

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import { Toaster } from "@/components/ui/toaster"
+import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -21,6 +23,8 @@ export default function RootLayout({
     <html lang="ko">
       <body className={inter.className}>
         {children}
+        <Analytics />
+        <SpeedInsights />
         <Toaster />
       </body>
     </html>
